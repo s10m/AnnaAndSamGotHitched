ed1908678b4f5f83bb09b10b421eff6966b4d947
Sam Townsend <samjtowns@gmail.com>
2017-08-14 23:29:17 +0100

    Added some fake memories

---
+++ AnnaAndSamGotHitched/ViewModels/Memory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnnaAndSamGotHitched.ViewModels
+{
+    public class MemoryViewModel
+    {
+        public MemoryViewModel(string p_Note)
+        {
+            Note = p_Note;
+        }
+        public bool HasImage { get; set; }
+
+        public Guid? ImageRef { get; set; }
+
+        public string Note { get; set; }
+    }
+
+    public class MemoryListingViewModel
+    {
+        public MemoryListingViewModel()
+        {
+            Memories = new List<MemoryViewModel>
+            {
+                new MemoryViewModel("Pudding and Stinky"),
+                new MemoryViewModel("Looking good"),
+                new MemoryViewModel("We'll never forget today"),
+                new MemoryViewModel("I love you pudding"),
+            };
+        }
+
+        public List<MemoryViewModel> Memories { get; set; }
+    }
+}
